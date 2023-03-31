1de1768760ab761826f8051d891762c6033e3cd4
DESKTOP-A961FNP\Даниил <klintcevd@mail.ru>
2023-04-01 00:56:16 +0400

    Code review

--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -28,6 +28,148 @@ export default function HomePage() {
           </div>
         </div>
       </nav>
+      
+	
+	<div class="container mt-5">
+		<div class="row">
+
+			<div class="col-md-3">
+				<div class="product">
+					<div class="image">
+						<img src="http://placehold.it/300x400" alt=""/>
+					</div>
+
+
+					<div class="info">
+						<h3>Марка и модель</h3>
+						<ul class="rating">
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star-half-outline"></ion-icon></li>
+						</ul>
+						<div class="info-price">
+							<span class="price">2 000 000<small>₽</small></span>
+							<button class="add-to-favourites"><ion-icon name="heart-outline"></ion-icon></button>
+						</div>
+					</div>
+
+
+				</div>
+			</div>
+			
+
+			<div class="col-md-3">
+				<div class="product">
+					<div class="image">
+						<img src="http://placehold.it/300x400" alt=""/>
+					</div>
+
+
+					<div class="info">
+						<h3>Марка и модель</h3>
+						<ul class="rating">
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star-half-outline"></ion-icon></li>
+						</ul>
+						<div class="info-price">
+							<span class="price">2 000 000<small>₽</small></span>
+							<button class="add-to-favourites"><ion-icon name="heart-outline"></ion-icon></button>
+						</div>
+					</div>
+
+
+				</div>
+			</div>
+			
+
+			<div class="col-md-3">
+				<div class="product">
+					<div class="image">
+						<img src="http://placehold.it/300x400" alt=""/>
+					</div>
+
+
+					<div class="info">
+						<h3>Марка и модель</h3>
+						<ul class="rating">
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star-half-outline"></ion-icon></li>
+						</ul>
+						<div class="info-price">
+							<span class="price">2 000 000<small>₽</small></span>
+							<button class="add-to-favourites"><ion-icon name="heart-outline"></ion-icon></button>
+						</div>
+					</div>
+
+
+				</div>
+			</div>
+			
+
+			<div class="col-md-3">
+				<div class="product">
+					<div class="image">
+						<img src="http://placehold.it/300x400" alt=""/>
+					</div>
+
+
+					<div class="info">
+						<h3>Марка и модель</h3>
+						<ul class="rating">
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star-half-outline"></ion-icon></li>
+						</ul>
+						<div class="info-price">
+							<span class="price">2 000 000<small>₽</small></span>
+							<button class="add-to-favourites"><ion-icon name="heart-outline"></ion-icon></button>
+						</div>
+					</div>
+
+
+				</div>
+			</div>
+
+			<div class="col-md-3">
+				<div class="product">
+					<div class="image">
+						<img src="http://placehold.it/300x400" alt=""/>
+					</div>
+
+
+					<div class="info">
+						<h3>Марка и модель</h3>
+						<ul class="rating">
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star"></ion-icon></li>
+							<li><ion-icon name="star-half-outline"></ion-icon></li>
+						</ul>
+						<div class="info-price">
+							<span class="price">2 000 000<small>₽</small></span>
+							<button class="add-to-favourites"><ion-icon name="heart-outline"></ion-icon></button>
+						</div>
+					</div>
+
+
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	
     </>
   );
 }
