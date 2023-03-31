6579d339550b9d0579711a60c9baa7f5dc41d356
radik <rad.husainov2016@gmail.com>
2023-03-31 23:59:12 +0400

    fixed styles

--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -1,10 +1,32 @@
-import styles from "./style.module.css";
-
+import "./style.css";
 
 export default function HomePage() {
   return (
     <>
-        <a>Страница</a>
+      <nav class="navbar">
+        <div class="container">
+          <a href="#" class="navbar-brand">
+            Auto.Ru
+          </a>
+          <div class="navbar-wrap">
+            <ul class="navbar-menu">
+              <li>
+                <a href="#">О нас</a>
+              </li>
+              <li>
+                <a href="#">Новости</a>
+              </li>
+              <li>
+                <a href="#">Контакты</a>
+              </li>
+            </ul>
+
+            <a href="#" class="callback">
+              Личный кабинет
+            </a>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
