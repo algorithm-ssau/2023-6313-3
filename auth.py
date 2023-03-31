4f19e270e38a1952b76ee6ce5f1af52d32b4779c
Maksim Khrestinin <maksim.khrestinin@gmail.com>
2023-03-31 23:09:24 +0400

    Auth template

--- auth.py
+++ auth.py
@@ -2,14 +2,28 @@ from flask import Flask, request
 import json
 import hashlib
 
-import  authModel
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello guys'
+@app.route('/auth', methods=["POST"])
+def auth():
+    return "Awaiting Implementation"
 
+@app.route("/verify", methods=["POST"])
+def verify():
+    return "Awaiting Implementation"
+
+@app.route("/blacklist", methods=["GET", "POST"])
+def blacklist():
+    return "Awaiting Implementation"
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    return "Awaiting Implementation"
+
+@app.route("/client", methods=["GET", "POST"])
+def client():
+    return "Awaiting Implementation"
 
 if __name__ == '__main__':
     app.run()
