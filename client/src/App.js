15a593942e03429138036f591119b7f4800e6948
radik <rad.husainov2016@gmail.com>
2023-04-01 13:09:31 +0400

    extract navbar in component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import HomePage from "./Pages/Home";
+import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
     <>
+      <Navbar />
       <HomePage />
     </>
   );
--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -1,175 +1,196 @@
 import "./style.css";
 
 export default function HomePage() {
-  const j = "ahil";
   return (
     <>
-      <nav class="navbar">
-        <div class="container">
-          <a href="#" class="navbar-brand">
-            Auto.Ru
-          </a>
-          <div class="navbar-wrap">
-            <ul class="navbar-menu">
-              <li>
-                <a href="#">О нас</a>
-              </li>
-              <li>
-                <a href="#">Новости</a>
-              </li>
-              <li>
-                <a href="#">Контакты</a>
-              </li>
-            </ul>
+      <div class="container mt-5">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="product">
+              <div class="image">
+                <img src="http://placehold.it/300x400" alt="" />
+              </div>
 
-            <a href="#" class="callback">
-              Личный кабинет
-            </a>
+              <div class="info">
+                <h3>Марка и модель</h3>
+                <ul class="rating">
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star-half-outline"></ion-icon>
+                  </li>
+                </ul>
+                <div class="info-price">
+                  <span class="price">
+                    2 000 000<small>₽</small>
+                  </span>
+                  <button class="add-to-favourites">
+                    <ion-icon name="heart-outline"></ion-icon>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="product">
+              <div class="image">
+                <img src="http://placehold.it/300x400" alt="" />
+              </div>
+
+              <div class="info">
+                <h3>Марка и модель</h3>
+                <ul class="rating">
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star-half-outline"></ion-icon>
+                  </li>
+                </ul>
+                <div class="info-price">
+                  <span class="price">
+                    2 000 000<small>₽</small>
+                  </span>
+                  <button class="add-to-favourites">
+                    <ion-icon name="heart-outline"></ion-icon>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="product">
+              <div class="image">
+                <img src="http://placehold.it/300x400" alt="" />
+              </div>
+
+              <div class="info">
+                <h3>Марка и модель</h3>
+                <ul class="rating">
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star-half-outline"></ion-icon>
+                  </li>
+                </ul>
+                <div class="info-price">
+                  <span class="price">
+                    2 000 000<small>₽</small>
+                  </span>
+                  <button class="add-to-favourites">
+                    <ion-icon name="heart-outline"></ion-icon>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="product">
+              <div class="image">
+                <img src="http://placehold.it/300x400" alt="" />
+              </div>
+
+              <div class="info">
+                <h3>Марка и модель</h3>
+                <ul class="rating">
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star-half-outline"></ion-icon>
+                  </li>
+                </ul>
+                <div class="info-price">
+                  <span class="price">
+                    2 000 000<small>₽</small>
+                  </span>
+                  <button class="add-to-favourites">
+                    <ion-icon name="heart-outline"></ion-icon>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="product">
+              <div class="image">
+                <img src="http://placehold.it/300x400" alt="" />
+              </div>
+
+              <div class="info">
+                <h3>Марка и модель</h3>
+                <ul class="rating">
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star"></ion-icon>
+                  </li>
+                  <li>
+                    <ion-icon name="star-half-outline"></ion-icon>
+                  </li>
+                </ul>
+                <div class="info-price">
+                  <span class="price">
+                    2 000 000<small>₽</small>
+                  </span>
+                  <button class="add-to-favourites">
+                    <ion-icon name="heart-outline"></ion-icon>
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </nav>
-      
-	
-	<div class="container mt-5">
-		<div class="row">
-
-			<div class="col-md-3">
-				<div class="product">
-					<div class="image">
-						<img src="http://placehold.it/300x400" alt=""/>
-					</div>
-
-
-					<div class="info">
-						<h3>Марка и модель</h3>
-						<ul class="rating">
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star-half-outline"></ion-icon></li>
-						</ul>
-						<div class="info-price">
-							<span class="price">2 000 000<small>₽</small></span>
-							<button class="add-to-favourites"><ion-icon name="heart-outline"></ion-icon></button>
-						</div>
-					</div>
-
-
-				</div>
-			</div>
-			
-
-			<div class="col-md-3">
-				<div class="product">
-					<div class="image">
-						<img src="http://placehold.it/300x400" alt=""/>
-					</div>
-
-
-					<div class="info">
-						<h3>Марка и модель</h3>
-						<ul class="rating">
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star-half-outline"></ion-icon></li>
-						</ul>
-						<div class="info-price">
-							<span class="price">2 000 000<small>₽</small></span>
-							<button class="add-to-favourites"><ion-icon name="heart-outline"></ion-icon></button>
-						</div>
-					</div>
-
-
-				</div>
-			</div>
-			
-
-			<div class="col-md-3">
-				<div class="product">
-					<div class="image">
-						<img src="http://placehold.it/300x400" alt=""/>
-					</div>
-
-
-					<div class="info">
-						<h3>Марка и модель</h3>
-						<ul class="rating">
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star-half-outline"></ion-icon></li>
-						</ul>
-						<div class="info-price">
-							<span class="price">2 000 000<small>₽</small></span>
-							<button class="add-to-favourites"><ion-icon name="heart-outline"></ion-icon></button>
-						</div>
-					</div>
-
-
-				</div>
-			</div>
-			
-
-			<div class="col-md-3">
-				<div class="product">
-					<div class="image">
-						<img src="http://placehold.it/300x400" alt=""/>
-					</div>
-
-
-					<div class="info">
-						<h3>Марка и модель</h3>
-						<ul class="rating">
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star-half-outline"></ion-icon></li>
-						</ul>
-						<div class="info-price">
-							<span class="price">2 000 000<small>₽</small></span>
-							<button class="add-to-favourites"><ion-icon name="heart-outline"></ion-icon></button>
-						</div>
-					</div>
-
-
-				</div>
-			</div>
-
-			<div class="col-md-3">
-				<div class="product">
-					<div class="image">
-						<img src="http://placehold.it/300x400" alt=""/>
-					</div>
-
-
-					<div class="info">
-						<h3>Марка и модель</h3>
-						<ul class="rating">
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star"></ion-icon></li>
-							<li><ion-icon name="star-half-outline"></ion-icon></li>
-						</ul>
-						<div class="info-price">
-							<span class="price">2 000 000<small>₽</small></span>
-							<button class="add-to-favourites"><ion-icon name="heart-outline"></ion-icon></button>
-						</div>
-					</div>
-
-
-				</div>
-			</div>
-
-		</div>
-	</div>
-
-	
+      </div>
     </>
   );
 }
