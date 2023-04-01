95b28f9d4b10e5ff1f2a97a91f5a580965b23916
radik <rad.husainov2016@gmail.com>
2023-04-01 23:11:00 +0400

    extract card in component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import HomePage from "./Pages/Home";
 import Navbar from "./components/Navbar.js";
+import Card from "./components/Card.js";
 
 function App() {
   return (
     <>
       <Navbar />
-      <HomePage />
+      <HomePage>
+        <Card title={"Марка и модель"} price={2000000} />
+        <Card title={"Марка и модель"} price={2000000} />
+        <Card title={"Марка и модель"} price={2000000} />
+        <Card title={"Марка и модель"} price={2000000} />
+      </HomePage>
     </>
   );
 }
