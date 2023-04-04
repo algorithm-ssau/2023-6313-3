e840e35f8bf059e72c706a117726a0a9dc5e749b
radik <rad.husainov2016@gmail.com>
2023-04-04 23:29:49 +0400

    completed pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,5 @@
-import HomePage from "./Pages/Home";
-import Navbar from "./components/Navbar.js";
-import Card from "./components/Card.js";
 function App() {
-  return (
-    <>
-      <Navbar />
-      <HomePage>
-        <Card title={"Марка и модель"} price={"2 000 000"} />
-        <Card title={"Марка и модель"} price={"2 000 000"} />
-        <Card title={"Марка и модель"} price={"2 000 000"} />
-        <Card title={"Марка и модель"} price={"2 000 000"} />
-      </HomePage>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
