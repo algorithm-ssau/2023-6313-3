a72ce9744cb112496a97d4be42cb16d4642f6082
Maksim Khrestinin <maksim.khrestinin@gmail.com>
2023-04-10 14:49:46 +0400

    Change templates' structure

--- server/Auth service/auth.py
+++ server/Auth service/auth.py
@@ -5,22 +5,17 @@ import hashlib
 
 app = Flask(__name__)
 
+# Authentication method (client exists)
 @app.route('/auth', methods=["POST"])
 def auth():
     return "Awaiting Implementation"
 
+# Verifying the signed JWT token
 @app.route("/verify", methods=["POST"])
 def verify():
     return "Awaiting Implementation"
 
-@app.route("/blacklist", methods=["GET", "POST"])
-def blacklist():
-    return "Awaiting Implementation"
-
-@app.route("/logout", methods=["POST"])
-def logout():
-    return "Awaiting Implementation"
-
+# Here we set up the hashing algorithm to create a client_id and client_password for us for first time use
 @app.route("/client", methods=["GET", "POST"])
 def client():
     return "Awaiting Implementation"
