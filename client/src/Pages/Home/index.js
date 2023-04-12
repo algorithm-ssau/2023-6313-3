b1e524d3dfe07f3adc408bde309779b591a5430c
DESKTOP-A961FNP\Даниил <klintcevd@mail.ru>
2023-04-12 21:21:08 +0400

    Footer

--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -18,6 +18,54 @@ export default function HomePage() {
           <Card title={"Марка и модель"} price={2000000} />
         </div>
       </div>
+      <div className={styles["hr"]}></div>
+      <footer className={styles["footer"]}>
+        
+         
+        <a href="#" className={styles["brand"]}>
+          Auto.Ru
+        </a>
+
+        <ul className={styles["social"]}>
+          <li>
+            <a href="#">
+              <ion-icon name="logo-facebook"></ion-icon>
+            </a>
+            <a href="#">
+              <ion-icon name="logo-twitter"></ion-icon>
+            </a>
+            <a href="#">
+              <ion-icon name="logo-linkedin"></ion-icon>
+            </a>
+            <a href="#">
+              <ion-icon name="logo-instagram"></ion-icon>
+            </a>
+          </li>
+        </ul>
+
+        <ul className={styles["menu"]}>
+          <li>
+            <a href="#">Главная</a>
+          </li>
+          <li>
+            <a href="#">О проекте</a>
+          </li>
+          <li>
+            <a href="#">Помощь</a>
+          </li>
+          <li>
+            <a href="#">Разместить рекламу</a>
+          </li>
+          <li>
+            <a href="#">Контакты</a>
+          </li>
+        </ul>
+
+        <p>©2023 Auto.Ru | All Rights Reserved</p>
+
+       
+      </footer>
+      <div className={styles["hr"]}></div>
     </div>
   );
 }
