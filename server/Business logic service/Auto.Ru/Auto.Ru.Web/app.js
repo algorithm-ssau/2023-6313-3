a997b76097d856b23db9b53f9d95b921bc4061ef
Bogdan Shaposhnikov <shaposhnikov@mercury.office>
2023-04-16 14:13:56 +0400

    Fixed configs and packages

--- server/Business logic service/Auto.Ru/Auto.Ru.Web/app.js
+++ server/Business logic service/Auto.Ru/Auto.Ru.Web/app.js
@@ -8,6 +8,30 @@ app.get('/', (req, res) => {
 
 module.exports.env = app.get('env');
 
+const config = require('./config/config.json').development
+
+const Sequelize = require('sequelize');
+
+  const sequelize = new Sequelize('Auto.Ru', 'auto-ru-client', '12345', {
+    host: config.host,
+    dialect: "mssql",
+    dialectOptions: {
+        options: {
+          trustServerCertificate: true
+        }, 
+      }
+  });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
 })
