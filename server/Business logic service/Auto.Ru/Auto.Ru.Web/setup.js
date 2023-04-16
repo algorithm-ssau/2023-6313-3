bab7162c661ae1d95a44379639ffb770d28e613e
Bogdan Shaposhnikov <shaposhnikov@mercury.office>
2023-04-16 14:13:56 +0400

    Fixed configs and packages

--- server/Business logic service/Auto.Ru/Auto.Ru.Web/setup.js
+++ server/Business logic service/Auto.Ru/Auto.Ru.Web/setup.js
@@ -4,16 +4,8 @@ const Sequelize = require("sequelize");
 var config = dbConfig.development;
 
 const sequelize = new Sequelize(config, {
-  host: config.HOST,
-  dialect: config.dialect,
-  operatorsAliases: false,
-
-  pool: {
-    max: config.pool.max,
-    min: config.pool.min,
-    acquire: config.pool.acquire,
-    idle: config.pool.idle
-  }
+  host: config.host,
+  dialect: config.dialect
 });
 
 const db = {};
