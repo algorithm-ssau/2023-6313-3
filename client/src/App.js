cec2e0e003dcd8b5bcc124c400b22a89e651a3ce
radik <linuxoid.ssh@gmail.com>
2023-04-26 13:39:19 +0400

    feat(front): add car page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/Home";
 import RegisterPage from "./Pages/Register";
 import ProfilePage from "./Pages/Profile";
+import CarPage from "./Pages/Car";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path={"/home"} element={<HomePage />} />
       <Route path={"/register"} element={<RegisterPage />} />
       <Route path={"/profile"} element={<ProfilePage />} />
+      <Route path={"/car"} element={<CarPage />} />
     </Routes>
   );
 }
