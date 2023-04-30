aaf106ae310477af54a664f89a9c6713b39ae8bf
radik <124812541+husainof@users.noreply.github.com>
2023-04-30 23:17:13 +0300

    Revert "Updated profile page"

--- client/src/Pages/Car/fakeImages.js
+++
@@ -1,5 +0,0 @@
-export const images = [
-    { url: "https://sun1-26.userapi.com/impg/2zfJjJVaHMZIbUCrP62d6eDYTgF8Hg3ISV9gZg/athNrKulBB8.jpg?size=1002x1002&quality=96&sign=2fce36d04e6775146b92d893186bd717&type=album" },
-    { url: "https://sun1-87.userapi.com/impg/Mdv6QB1dduLNBYpptZpo2Y7eJ9WYk4-3AVMIyg/wMybKrc3tKQ.jpg?size=1097x1097&quality=96&sign=05405b4d8d71249751d5fe4ffb43656c&type=album" },
-    { url: "https://sun1-25.userapi.com/impg/Z0MpdSZluqyFhlEJcA_lOgbBYlcB5gRudTQ1MA/CQNeJ8gtNww.jpg?size=969x969&quality=96&sign=cf350ff42dbc7b7277afd190cee71b33&type=album" },
-  ];
