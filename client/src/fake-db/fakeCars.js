3d734ec49e1e3475da4e8e476d8b166a9ab794c8
radik <linuxoid.ssh@gmail.com>
2023-05-02 08:56:27 +0400

    fix(front): fix home-page

---
+++ client/src/fake-db/fakeCars.js
@@ -0,0 +1,51 @@
+export const cars = [
+    {
+        id: 1,
+        name: "Changan UNI-K",
+        price: 4000,
+        imageUrl: "https://avatars.mds.yandex.net/get-autoru-vos/1871539/6d3edaa149ef4f992d71fb71d4dcabd9/1200x900n",
+
+    },
+    {
+        id: 2,
+        name: "Porsche Panamera GTS",
+        price: 3000,
+        imageUrl: "https://avatars.mds.yandex.net/get-autoru-vos/7068340/b96ff9cad1dac94f3e1feef760d6ac0e/1200x900n",
+    },
+    {
+        id: 3,
+        name: "Geely Monjaro",
+        price: 15000,
+        imageUrl: "https://yastatic.net/naydex/autoru/gRqI12935/1156a0je/TVywHQmywdQ0J5TYgjVeCdz40kyuhBdnGhTYtsHRGIHfvJdzacsSmJuz32RF4Ne0miVRm8zzLrGxShVwi2_ZP7htlRwZ5tXQsaKAuXPAyhyJjFOty0BWl4m2bnTCo6qD3Ho0gXjutv1qZ6uik2AjxjwgsOxcEzqRhAhRQmqrfTh9zPJNWNKgMGX3hLxBlE24zCrUH2xXoqe6dx32AtE48D1qtzP6XJdt_HS6YQGHUOfOAZM9TEBoouWZEWL7SQxL3F1gE",
+    },
+    {
+        id: 4,
+        name: "Opel Astra",
+        price: 14000,
+        imageUrl: "https://yastatic.net/naydex/autoru/gRqI12935/1156a0je/TVywHQmywdQ0J5TYgjVeCdz40kyuhBdnGhTYtsHRGIHfvJdzacsSmIvT72Tl0NeBmgCEzunTW3ThTyVl-xp5Go14lRwJhtDwgeLFzFYwChyJjFOty0BWl4m2bnTCo6qD3Ho0gXjutv1qZ6uik2AjxjwgsOxcEzqRhAhRQmqrfTh9zPJNWNKgMGX3hLxBlE24zCrUH2xXoqe6dx32AtE48D1qtzP6XJdt_HS6YQGHUOfOAZM9TEBoouWZEWL7SQxL3F1gE",
+    },
+    {
+        id: 5,
+        name: "Toyota Land Cruiser Prado",
+        price: 13000,
+        imageUrl: "https://avatars.mds.yandex.net/get-verba/997355/2a0000016b0370afac01a97d14c78fbc7a24/1200x900n",
+    },
+    {
+        id: 6,
+        name: "Volkswagen Tiguan Allspace II",
+        price: 12000,
+        imageUrl: "https://avatars.mds.yandex.net/get-verba/997355/2a0000017b16d6af8cd568f8bd9d0d5e9bb1/auto_main",
+    },
+    {
+        id: 7,
+        name: "Haval F7",
+        price: 11000,
+        imageUrl: "https://avatars.mds.yandex.net/get-autoru-vos/2079554/b15759f96000b6550ff592b558553ec9/1200x900n",
+    },
+    {
+        id: 8,
+        name: "Mitsubishi Pajero Sport III",
+        price: 10000,
+        imageUrl: "https://yastatic.net/naydex/autoru/gRqI12935/1156a0je/TVywHQmywdQ0J5TYgjVeCdz40kyuhBdnGhTYtsHRGIHfvJdzacsS2Auzj-R14NexmjD0u-yGXhS071UV6w88f90ooGxZtqVlsfLVjAbAehyJjFOty0BWl4m2bnTCo6qD3Ho0gXjutv1qZ6uik2AjxjwgsOxcEzqRhAhRQmqrfTh9zPJNWNKgMGX3hLxBlE24zCrUH2xXoqe6dx32AtE48D1qtzP6XJdt_HS6YQGHUOfOAZM9TEBoouWZEWL7SQxL3F1gE",
+    },
+]
