d8292f07e507090e057667a33eadab847ec36cb5
Maksim Khrestinin <maksim.khrestinin@gmail.com>
2023-05-08 18:28:49 +0400

    Fix some implementation

--- server/Auth service/JWT/JWTGenerator.py
+++ server/Auth service/JWT/JWTGenerator.py
@@ -1,54 +1,38 @@
 from fastapi import FastAPI, status, HTTPException
 from jose import JWTError, jwt
-from pydantic import BaseModel
 from datetime import datetime, timedelta
 
 
 SECRET_KEY = "adbd35fdfb55f0f0a431b7788707256dbd1e69be93f26aafb1d69317e3a613ef"
-
 ALGORITHM = "HS256"
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
+TIME_DELTA = 15
+data = {
+    'username': 'user',
+    'password': 'password',
+    'email': 'smth@gmail.com'
+}
 
 app = FastAPI()
 
-
-# this function will create the token
-# for particular data
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    # expire time of the token
-    expire = datetime.utcnow() + timedelta(minutes=15)
+    expire = datetime.utcnow() + timedelta(minutes=TIME_DELTA)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-    # return the generated token
     return encoded_jwt
 
-
-# the endpoint to get the token
-@app.get("/get_token")
-async def get_token():
-    # data to be signed using token
-    data = {
-        'info': 'secret information',
-        'from': 'GFG'
-    }
+@app.post("/refresh")
+async def get_token(data: dict):
     token = create_access_token(data=data)
-    return {'token': token}
+    return {'access-token': token}
 
 
-# the endpoint to verify the token
-@app.post("/verify_token")
+@app.post("/checking")
 async def verify_token(token: str):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload
+        jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return {'success': 'true'}
     except JWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-        )
+        return {'success': 'false'}
