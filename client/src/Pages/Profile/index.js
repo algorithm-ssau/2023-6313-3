4fa576ed9a274d8a0995f73760b80f61dbddf2b6
Klintsev Daniil <klintcevd@mail.ru>
2023-05-10 14:45:10 +0400

    Profile

--- client/src/Pages/Profile/index.js
+++ client/src/Pages/Profile/index.js
@@ -16,20 +16,34 @@ export default function ProfilePage() {
             alt=""
           />
           <h1 className={styles["name"]}>Василий</h1>
-          <h1 className={styles["data1"]}>Телефон</h1>
+          <h1 className={styles["number"]}>Телефон</h1>
 
-          <h1 className={styles["data2"]}>e-mail</h1>
+          <h1 className={styles["mail"]}>e-mail</h1>
+          <h1 className={styles["add"]}>Добавить объявление</h1>
+          
           <a href="#" className={styles["info"]}>
             О пользователе
           </a>
         </div>
 
-        <div className={styles["container_center"]}>
-          
-        </div>
-
-        <div className={styles["container"]}>
+        <div className={styles["container_like"]}>
           <h1 className={styles["favourites"]}>Избранное</h1>
+
+          <div className={styles["favourite"]}>
+            <div className={styles["photo-favourite"]}>
+              <img
+                src="http://placehold.it/300x200"
+                alt=""
+              />
+            </div>
+            <div className={styles["info-favourite"]}>
+              <h3>Марка и модель</h3>
+              <h3>2.000.000</h3>
+            </div>
+            <div className={styles["buttons-favourite"]}>
+              <h1 className={styles["best-price"]}>Справедливая цена</h1>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
