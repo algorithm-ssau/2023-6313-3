328ab9f89d7e4a8cf5e250d0635ca24079ac04d5
radik <linuxoid.ssh@gmail.com>
2023-05-12 19:09:20 +0400

    fix after merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 
-import HomePage from "./Pages/Home";
-import RegisterPage from "./Pages/Register";
-import ProfilePage from "./Pages/Profile";
-import CarPage from "./Pages/Car";
+import HomePage from './Pages/Home';
+import RegisterPage from './Pages/Register';
+import ProfilePage from './Pages/Profile';
+import CarPage from './Pages/Car';
+import RequireAuth from './utils/requireAuth.jsx';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/cars" element={<HomePage />} />
-      <Route path="/cars/:id" element={<CarPage />} />
+      <Route path='/' element={<HomePage />} />
+      <Route path='/register' element={<RegisterPage />} />
+      <Route element={<RequireAuth />}>
+        <Route path='/profile' element={<ProfilePage />} />
+      </Route>
+      <Route path='/cars' element={<HomePage />} />
+      <Route path='/cars/:id' element={<CarPage />} />
     </Routes>
   );
 }
