4e94d7f7a38adabcb68fdbbe59298dd7678ce63a
Klintsev Daniil <klintcevd@mail.ru>
2023-05-14 13:51:03 +0400

    Profile

--- client/src/Pages/Profile/index.js
+++ client/src/Pages/Profile/index.js
@@ -19,8 +19,7 @@ export default function ProfilePage() {
           <h1 className={styles["number"]}>Телефон</h1>
 
           <h1 className={styles["mail"]}>e-mail</h1>
-          <h1 className={styles["add"]}>Добавить объявление</h1>
-          
+
           <a href="#" className={styles["info"]}>
             О пользователе
           </a>
@@ -29,21 +28,66 @@ export default function ProfilePage() {
         <div className={styles["container_like"]}>
           <h1 className={styles["favourites"]}>Избранное</h1>
 
-          <div className={styles["favourite"]}>
-            <div className={styles["photo-favourite"]}>
-              <img
-                src="http://placehold.it/300x200"
-                alt=""
-              />
+          <div className={styles["wrap-favourite"]}>
+
+            <div className={styles["favourite"]}>
+              <div className={styles["photo-favourite"]}>
+                <img src="http://placehold.it/300x200" alt="" />
+              </div>
+              <div className={styles["info-favourite"]}>
+                <h3>Марка и модель</h3>
+                <h3>2.000.000</h3>
+              </div>
+              <div className={styles["buttons-favourite"]}>
+                <h1 className={styles["best-price"]}>Справедливая цена</h1>
+              </div>
             </div>
-            <div className={styles["info-favourite"]}>
-              <h3>Марка и модель</h3>
-              <h3>2.000.000</h3>
+
+            <div className={styles["favourite"]}>
+              <div className={styles["photo-favourite"]}>
+                <img src="http://placehold.it/300x200" alt="" />
+              </div>
+              <div className={styles["info-favourite"]}>
+                <h3>Марка и модель</h3>
+                <h3>2.000.000</h3>
+              </div>
+              <div className={styles["buttons-favourite"]}>
+                <h1 className={styles["best-price"]}>Справедливая цена</h1>
+              </div>
             </div>
-            <div className={styles["buttons-favourite"]}>
-              <h1 className={styles["best-price"]}>Справедливая цена</h1>
+
+            <div className={styles["favourite"]}>
+              <div className={styles["photo-favourite"]}>
+                <img src="http://placehold.it/300x200" alt="" />
+              </div>
+              <div className={styles["info-favourite"]}>
+                <h3>Марка и модель</h3>
+                <h3>2.000.000</h3>
+              </div>
+              <div className={styles["buttons-favourite"]}>
+                <h1 className={styles["best-price"]}>Справедливая цена</h1>
+              </div>
             </div>
+
+            <div className={styles["favourite"]}>
+              <div className={styles["photo-favourite"]}>
+                <img src="http://placehold.it/300x200" alt="" />
+              </div>
+              <div className={styles["info-favourite"]}>
+                <h3>Марка и модель</h3>
+                <h3>2.000.000</h3>
+              </div>
+              <div className={styles["buttons-favourite"]}>
+                <h1 className={styles["best-price"]}>Справедливая цена</h1>
+              </div>
+            </div>
+            
+
+            
+
           </div>
+
+          
         </div>
       </div>
       <Footer />
