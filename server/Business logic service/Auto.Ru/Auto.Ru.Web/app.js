876ee6cd6798d0025e37aa80e18802f3bf3abbe2
= <shaposhnik_off@mail.ru>
2023-05-20 14:27:13 +0400

    Added exception filter

--- server/Business logic service/Auto.Ru/Auto.Ru.Web/app.js
+++ server/Business logic service/Auto.Ru/Auto.Ru.Web/app.js
@@ -5,13 +5,22 @@ const port = 1000;
 
 module.exports.env = app.get("env");
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
-
 const cars = require("./routers/carsRouter");
+
+app.use((err, req, res, next) => {
+  if (! err) {
+      return next();
+  }
+
+  res.status(500);
+  res.send('500: Internal server error');
+});
 
 app.use(cors());
 
 app.use(express.json());
 app.use("/api/cars", cars);
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
