37335cbc478794ae5534895732ed4296e2eb3d23
LAPTOP-LSU2V0PR\klint <klintcevd@mail.ru>
2023-05-20 18:18:34 +0400

    Fix add car page

--- client/src/Pages/AddCar/index.jsx
+++ client/src/Pages/AddCar/index.jsx
@@ -12,6 +12,7 @@ import {
   Card,
   Heading,
   Select,
+  VStack,
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
@@ -66,144 +67,156 @@ const AddCarPage = () => {
 
   return (
     <div className='container mt-5'>
-      <h1>Add Car</h1>
       <Form onSubmit={handleSubmit}>
-        <Form.Group controlId='makeModel'>
-          <Form.Label>Марка и модель</Form.Label>
-          <Form.Control
-            type='text'
-            name='makeModel'
-            value={formValues.makeModel}
-            onChange={handleSelectChange}
-            required
-          />
-        </Form.Group>
+        <VStack
+          spacing={6}
+          align='stretch'
+          maxW='800px'
+          m='auto'
+          fontSize='20px'
+        >
+          <Form.Group controlId='makeModel'>
+            <Form.Label>Марка и модель</Form.Label>
+            <Form.Control
+              type='text'
+              name='makeModel'
+              value={formValues.makeModel}
+              onChange={handleSelectChange}
+              required
+            />
+          </Form.Group>
 
-        <Form.Group controlId='price'>
-          <Form.Label>Цена</Form.Label>
-          <Form.Control
-            type='number'
-            name='price'
-            value={formValues.price}
-            onChange={handleSelectChange}
-            required
-          />
-        </Form.Group>
+          <Form.Group controlId='price'>
+            <Form.Label>Цена</Form.Label>
+            <Form.Control
+              type='number'
+              name='price'
+              value={formValues.price}
+              onChange={handleSelectChange}
+              required
+            />
+          </Form.Group>
 
-        <Form.Group controlId='imageUrl'>
-          <Form.Label>Фото</Form.Label>
-          <Form.Control
-            type='text'
-            name='imageUrl'
-            value={formValues.imageUrl}
-            onChange={handleSelectChange}
-            required
-          />
-        </Form.Group>
+          <Form.Group controlId='imageUrl'>
+            <Form.Label>Фото</Form.Label>
+            <Form.Control
+              type='text'
+              name='imageUrl'
+              value={formValues.imageUrl}
+              onChange={handleSelectChange}
+              required
+            />
+          </Form.Group>
 
-        <Form.Group controlId='year'>
-          <Form.Label>Год</Form.Label>
-          <Form.Control
-            type='text'
-            name='year'
-            value={formValues.year}
-            onChange={handleSelectChange}
-            required
-          />
-        </Form.Group>
+          <Form.Group controlId='year'>
+            <Form.Label>Год</Form.Label>
+            <Form.Control
+              type='text'
+              name='year'
+              value={formValues.year}
+              onChange={handleSelectChange}
+              required
+            />
+          </Form.Group>
 
-        <Form.Group controlId='mileage'>
-          <Form.Label>Пробег</Form.Label>
-          <Form.Control
-            type='text'
-            name='mileage'
-            value={formValues.mileage}
-            onChange={handleSelectChange}
-            required
-          />
-        </Form.Group>
+          <Form.Group controlId='mileage'>
+            <Form.Label>Пробег</Form.Label>
+            <Form.Control
+              type='text'
+              name='mileage'
+              value={formValues.mileage}
+              onChange={handleSelectChange}
+              required
+            />
+          </Form.Group>
 
-        <Form.Group controlId='color'>
-          <Form.Label>Цвет</Form.Label>
-          <Form.Control
-            type='text'
-            name='color'
-            value={formValues.color}
-            onChange={handleSelectChange}
-            required
-          />
-        </Form.Group>
+          <Form.Group controlId='color'>
+            <Form.Label>Цвет</Form.Label>
+            <Form.Control
+              type='text'
+              name='color'
+              value={formValues.color}
+              onChange={handleSelectChange}
+              required
+            />
+          </Form.Group>
 
-        <Form.Group controlId='colorengineVolume'>
-          <Form.Label>Оъём двигателя</Form.Label>
-          <Form.Control
-            type='text'
-            name='engineVolume'
-            value={formValues.engineVolume}
-            onChange={handleSelectChange}
-            required
-          />
-        </Form.Group>
+          <Form.Group controlId='colorengineVolume'>
+            <Form.Label>Объём двигателя</Form.Label>
+            <Form.Control
+              type='text'
+              name='engineVolume'
+              value={formValues.engineVolume}
+              onChange={handleSelectChange}
+              required
+            />
+          </Form.Group>
 
-        <Form.Group controlId='enginePowers'>
-          <Form.Label>Мощность</Form.Label>
-          <Form.Control
-            type='text'
-            name='enginePowers'
-            value={formValues.enginePowers}
-            onChange={handleSelectChange}
-            required
-          />
-        </Form.Group>
+          <Form.Group controlId='enginePowers'>
+            <Form.Label>Мощность</Form.Label>
+            <Form.Control
+              type='text'
+              name='enginePowers'
+              value={formValues.enginePowers}
+              onChange={handleSelectChange}
+              required
+            />
+          </Form.Group>
 
+          {/* исправить */}
+          <Form.Group controlId='leftSteeringWheel'>
+            <Form.Label>Левое рулевое колесо</Form.Label>
+            <Form.Control
+              name='leftSteeringWheel'
+              value={formValues.leftSteeringWheel}
+              onChange={handleSelectChange}
+              required
+            />
+          </Form.Group>
 
-        {/* исправить */}
-        <Form.Group controlId='leftSteeringWheel'> 
-          <Form.Label>Левое рулевое колесо</Form.Label>
-          <Form.Control
-            type='text'
-            name='leftSteeringWheel'
-            value={formValues.leftSteeringWheel}
-            onChange={handleSelectChange}
-            required
-          />
-        </Form.Group>
+          <Form.Group controlId='transmission'>
+            <Form.Label>Коробка передач</Form.Label>
+            <Form.Control
+              as='select'
+              name='transmission'
+              value={formValues.transmission}
+              onChange={handleSelectChange}
+              required
+            >
+              <option value=''>Выбрать</option>
+              <option value='Manual'>Механика</option>
+              <option value='Automatic'>Автомат</option>
+              <option value='Variator'>Вариатор</option>
+              <option value='Robot'>Робот</option>
+            </Form.Control>
+          </Form.Group>
 
-        <Form.Group controlId='transmission'>
-          <Form.Label>Коробка передач</Form.Label>
-          <Form.Control
-            as='select'
-            name='transmission'
-            value={formValues.transmission}
-            onChange={handleSelectChange}
-            required
+          <Form.Group controlId='gear'>
+            <Form.Label>Привод</Form.Label>
+            <Form.Control
+              as='select'
+              name='gear'
+              value={formValues.drive}
+              onChange={handleSelectChange}
+              required
+            >
+              <option value=''>Выбрать</option>
+              <option value='Front-wheel drive'>Передний привод</option>
+              <option value='Rear-wheel drive'>Задний привод</option>
+              <option value='Full'>Полный привод</option>
+            </Form.Control>
+          </Form.Group>
+        </VStack>
+
+        <Center mt={30}>
+          <Button
+            variant='primary'
+            type='submit'
+            class='btn btn-primary btn-lg'
           >
-            <option value=''>Select Transmission</option>
-            <option value='Manual'>Manual</option>
-            <option value='Automatic'>Automatic</option>
-            <option value='CVT'>CVT</option>
-          </Form.Control>
-        </Form.Group>
-
-        <Form.Group controlId='gear'>
-          <Form.Label>Привод</Form.Label>
-          <Form.Control
-            as='select'
-            name='gear'
-            value={formValues.drive}
-            onChange={handleSelectChange}
-            required
-          >
-            <option value=''>Select Drive</option>
-            <option value='Front-wheel drive'>Front-wheel drive</option>
-            <option value='Rear-wheel drive'>Rear-wheel drive</option>
-            <option value='Full'>Full</option>
-          </Form.Control>
-        </Form.Group>      
-     
-        <Button variant='primary' type='submit'>
-          Submit
-        </Button>
+            Опубликовать
+          </Button>
+        </Center>
       </Form>
     </div>
   );
