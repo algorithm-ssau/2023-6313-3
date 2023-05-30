dc3265e7e3cc2745615aa42a6366cf427c53165a
radik <linuxoid.ssh@gmail.com>
2023-05-30 19:27:34 +0400

    fix: set local images

--- client/src/Pages/AboutUs/index.jsx
+++ client/src/Pages/AboutUs/index.jsx
@@ -2,16 +2,18 @@ import styles from './style.module.css';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 
+import headerPic from './about-page1.jpg';
+import pic1 from './about-page2.jpg';
+import pic2 from './about-page3.jpg';
+import pic3 from './about-page4.jpg';
+import pic4 from './about-page5.jpg';
+
 export default function ProfilePage() {
   return (
     <div className={styles['body']}>
       <Navbar />
       <div className={styles['container']}>
-        <img
-          src='https://sun9-24.userapi.com/impg/WvoAUw9iRYZLl4l9kl5QvnDHqaSoFrH83Sia4g/ICwe2IaNj2E.jpg?size=1280x800&quality=96&sign=dbe17eb77898e67c2a50bc8502410ab3&type=album'
-          alt='loading'
-          width='100%'
-        ></img>
+        <img src={headerPic} alt='loading' width='100%' />
         <div className={styles['about']}>
           <h1>О КОМПАНИИ</h1>
         </div>
@@ -36,20 +38,10 @@ export default function ProfilePage() {
       </div>
 
       <div className={styles['collage']}>
-        <img
-          src='https://sun9-70.userapi.com/impg/Q0P2Lp_wiXjbw578mKDC56AolmW9aj6_kmVgFQ/6raJSBzNhfA.jpg?size=468x714&quality=96&sign=7478b78458d713ec0ac586bfdd7eb331&type=album'
-          alt='Photo 1'
-        />
+        <img src={pic1} alt='Photo 1' />
         <div className={styles['photos-right']}>
-          <img
-            className={styles['col-photo']}
-            src='https://gamerwall.pro/uploads/posts/2022-06/1655895053_43-gamerwall-pro-p-mersedes-osenyu-oboi-47.jpg'
-            alt='Photo 2'
-          />
-          <img
-            src='https://sun9-54.userapi.com/impg/mzAAtTVBUYdyMuA63ITgHA-0682r1qYgMT_HyA/q8ClnTskqzA.jpg?size=1280x853&quality=96&sign=f8fa1212d642ae6397355e4adfddeb85&type=album'
-            alt='Photo 3'
-          />
+          <img className={styles['col-photo']} src={pic2} alt='Photo 2' />
+          <img src={pic3} alt='Photo 3' />
         </div>
       </div>
 
@@ -68,7 +60,7 @@ export default function ProfilePage() {
       </div>
 
       <div className={styles['last-photo']}>
-        <img src='https://agate.group/upload/img/about/04.jpg' alt='Photo 4' />
+        <img src={pic4} alt='Photo 4' />
       </div>
 
       <div className={styles['text-center-last']}>
